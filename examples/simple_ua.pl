47c7189e6ccb28c2bd9726bbbb75e6892e8227d1
Theo van Hoesel <Th.J.v.Hoesel@THEMA-MEDIA.nl>
2018-03-04 16:57:40 +0100

    Update example, so it reflects current changes.
    This example also demonstrates that one can pass in `Cache-Control` multiple
    times

--- examples/simple_ua.pl
+++ examples/simple_ua.pl
@@ -4,7 +4,10 @@ use LWP::UserAgent::Caching::Simple qw/get_from_json/;
 
 # $HTTP::Caching::DEBUG = 1;
 
-my $data = get_from_json(shift);
+my $data = get_from_json(shift,
+    'Cache-Control' => 'max-stale=3600',
+    'Cache-Control' => 'no-transform'
+);
 
 use Data::Dumper;
 print Dumper $data;
